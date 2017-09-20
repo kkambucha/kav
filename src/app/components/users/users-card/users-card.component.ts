6e9b456827ae46ad6182642c8d370ce52e5de5a0
Vadim Zakovryashin <v.zakovryashin@corp.mail.ru>
2017-09-20 15:12:23 +0300

    Fix users card

--- src/app/components/users/users-card/users-card.component.ts
+++ src/app/components/users/users-card/users-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { IUser } from '../../../../interfaces/user';
 import { UserService } from '../../../services/user.service';
